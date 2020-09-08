b15ad886b2082ceeaf7cc8e50afa08465453eb56
mosaad-cpu <70978432+mosaad-cpu@users.noreply.github.com>
2020-09-08 22:54:28 +0200

    First commit

---
+++ main.c
@@ -0,0 +1,80 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+   int first_num;
+   int sec_num ;
+   char  oprator;
+
+
+   printf("please enter your first number\n ");
+
+   scanf("%d",&first_num);
+    printf("please enter your oprator\n ");
+
+   scanf("%s",&oprator);
+   printf("please enter your sec number \n ");
+   scanf("%d",&sec_num);
+
+   if (oprator == '+'){
+
+    printf("your result is %d",first_num+sec_num);
+
+   }
+   else if (oprator == '-'){
+
+
+ printf("your result is %d",first_num-sec_num);
+
+
+   }
+   else if (oprator == '*'){
+
+
+ printf("your result is %d",first_num*sec_num);
+
+
+   }
+    else if (oprator == '/'){
+
+
+ printf("your result is %d",first_num/sec_num);
+
+
+   }
+   else if (oprator == '%'){
+
+
+ printf("your result is %d",first_num%sec_num);
+
+
+   }
+
+
+   else {
+
+    printf(" wrong oprator");
+
+
+
+   }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    return 0;
+}
